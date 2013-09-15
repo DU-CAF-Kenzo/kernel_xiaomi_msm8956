8e1e40a1608fb64746c089d86fd46b0be16d4a94
Vladimir Davydov <vdavydov@parallels.com>
2013-09-15 21:30:13 +0400

    sched/balancing: Prevent the reselection of a previous env.dst_cpu if some tasks are pinned
    Currently new_dst_cpu is prevented from being reselected actually, not
    dst_cpu. This can result in attempting to pull tasks to this_cpu twice.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/281f59b6e596c718dd565ad267fc38f5b8e5c995.1379265590.git.vdavydov@parallels.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7752,6 +7752,9 @@ more_balance:
 		 */
 		if ((env.flags & LBF_SOME_PINNED) && env.imbalance > 0) {
 
+			/* Prevent to re-select dst_cpu via env's cpus */
+			cpumask_clear_cpu(env.dst_cpu, env.cpus);
+
 			env.dst_rq	 = cpu_rq(env.new_dst_cpu);
 			env.dst_cpu	 = env.new_dst_cpu;
 			env.flags	&= ~LBF_SOME_PINNED;
