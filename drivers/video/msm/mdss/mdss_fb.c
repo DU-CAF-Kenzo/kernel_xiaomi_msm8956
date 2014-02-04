51e3626a8c899b7bd56feed54a4fe6d679521dce
Steve Kondik <shade@chemlab.org>
2014-02-04 02:12:53 -0800

    msm: Fix high load average from uninterruptible waits
     * The load average on all MSMs is currently hovering at around 14
       due to uninterruptible waits in various places.
     * Convert these waits to interruptible waits to bring the load
       back down to zero at idle.
    
    Change-Id: I14bad4af135b1594e235e2252cb484cb34d91054

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -3179,6 +3179,11 @@ static int __mdss_fb_display_thread(void *data)
 			continue;
 		}
 
+		if (ret) {
+			pr_info("%s: interrupted", __func__);
+			continue;
+		}
+
 		if (kthread_should_stop())
 			break;
 
