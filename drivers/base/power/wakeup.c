34d7953e2b8d9336678892cdf0886b0d40ec382d
jinqian <jinqian@google.com>
2015-04-13 14:12:47 -0700

    power: validate wakeup source before activating it
    A rogue wakeup source not registered in wakeup_sources list is not visible
    from wakeup_sources_stats_show. Check if the wakeup source is registered
    properly by looking at the timer struct.
    
    Signed-off-by: jinqian <jinqian@google.com>
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Kaushal Kumar <kaushalk@codeaurora.org>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -469,6 +469,10 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	if (!enable_bluesleep_ws && !strcmp(ws->name, "bluesleep"))
 		return;
 
+	if (WARN(wakeup_source_not_registered(ws),
+			"unregistered wakeup source\n"))
+		return;
+
 	/*
 	 * active wakeup source should bring the system
 	 * out of PM_SUSPEND_FREEZE state
