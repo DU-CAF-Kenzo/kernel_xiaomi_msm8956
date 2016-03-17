40674e56123f81374aee1375b19c1d984701faaf
Sunil Khatri <sunilkh@codeaurora.org>
2016-03-17 17:56:49 +0530

    msm: kgsl: Fix overflow in sharedmem read/write functions
    There could be possibility of integer overflow on adding
    sizeof(uint32_t) with uint64_t maximum offset bytes and result
    in a value smaller than uint64_t maximum memdesc size.
    
    CRs-Fixed: 988861
    Change-Id: Ifc3ec45297c2a29ad6f7d70dd0bd59238ac8cc3d
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -1004,8 +1004,8 @@ kgsl_sharedmem_readq(const struct kgsl_memdesc *memdesc,
 	if (offsetbytes % sizeof(uint32_t) != 0)
 		return -EINVAL;
 
-	WARN_ON(offsetbytes + sizeof(uint32_t) > memdesc->size);
-	if (offsetbytes + sizeof(uint32_t) > memdesc->size)
+	WARN_ON(offsetbytes > (memdesc->size - sizeof(uint32_t)));
+	if (offsetbytes > (memdesc->size - sizeof(uint32_t)))
 		return -ERANGE;
 
 	/*
@@ -1031,8 +1031,8 @@ kgsl_sharedmem_writeq(struct kgsl_device *device,
 	if (offsetbytes % sizeof(uint32_t) != 0)
 		return -EINVAL;
 
-	WARN_ON(offsetbytes + sizeof(uint32_t) > memdesc->size);
-	if (offsetbytes + sizeof(uint32_t) > memdesc->size)
+	WARN_ON(offsetbytes > (memdesc->size - sizeof(uint32_t)));
+	if (offsetbytes > (memdesc->size - sizeof(uint32_t)))
 		return -ERANGE;
 	kgsl_cffdump_write(device,
 		_lo_32(memdesc->gpuaddr + offsetbytes), src);
