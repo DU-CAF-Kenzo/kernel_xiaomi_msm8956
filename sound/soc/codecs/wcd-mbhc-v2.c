3ce35e2bb54655a3a494649a2685703ba9481fd3
Walter Yang <yandongy@codeaurora.org>
2017-07-10 16:47:35 +0000

    ASoC: wcd-mbhc: check HPH PA status before impedance detection
    If HPH PA is on, the result of impedance detection is not
    reliable. So check the HPH PA status before impedance detection.
    
    CRs-Fixed: 1100622
    Change-Id: I9c2063011247cca8105b39c913633cd7619a89eb
    Signed-off-by: Walter Yang <yandongy@codeaurora.org>

--- sound/soc/codecs/wcd-mbhc-v2.c
+++ sound/soc/codecs/wcd-mbhc-v2.c
@@ -532,6 +532,10 @@ static void wcd_mbhc_hs_elec_irq(struct wcd_mbhc *mbhc, int irq_type,
 static void wcd_mbhc_report_plug(struct wcd_mbhc *mbhc, int insertion,
 				enum snd_jack_types jack_type)
 {
+
+	struct snd_soc_codec *codec = mbhc->codec;
+	bool is_pa_on = false;
+
 	WCD_MBHC_RSC_ASSERT_LOCKED(mbhc);
 
 	pr_debug("%s: enter insertion %d hph_status %x\n",
