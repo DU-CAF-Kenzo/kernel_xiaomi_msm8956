285c7bf066a4d35bb801b594a282b900ea01d931
psndna88 <psndna88@gmail.com>
2017-07-22 11:21:28 +0530

    power: Add a simple API to query the charging state (on/off) at any point of time
    boolean: charging_detected()
    
    Changed Files:
    - drivers/power/qpnp-fg.c
    - include/linux/charging_state.h
    
    Signed-off-by: psndna88 <psndna88@gmail.com>
    Signed-off-by: MOVZX <movzx@yahoo.com>

--- drivers/power/qpnp-fg.c
+++ drivers/power/qpnp-fg.c
@@ -35,6 +35,7 @@
 #include <linux/string_helpers.h>
 #include <linux/alarmtimer.h>
 #include <linux/qpnp-revid.h>
+#include <linux/charging_state.h>
 
 /* Register offsets */
 
@@ -2304,6 +2305,13 @@ static int fg_inc_store_cycle_ctr(struct fg_chip *chip, int bucket)
 	return rc;
 }
 
+bool is_charging = false;
+
+bool charging_detected(void)
+{
+	return is_charging;
+}
+
 static void update_cycle_count(struct work_struct *work)
 {
 	int rc = 0, bucket, i;
@@ -3300,6 +3308,7 @@ static void status_change_work(struct work_struct *work)
 	}
 	if (chip->status == POWER_SUPPLY_STATUS_FULL ||
 			chip->status == POWER_SUPPLY_STATUS_CHARGING) {
+		is_charging = true;
 		if (!chip->vbat_low_irq_enabled) {
 			enable_irq(chip->batt_irq[VBATT_LOW].irq);
 			enable_irq_wake(chip->batt_irq[VBATT_LOW].irq);
@@ -3308,6 +3317,7 @@ static void status_change_work(struct work_struct *work)
 		if (capacity == 100)
 			fg_configure_soc(chip);
 	} else if (chip->status == POWER_SUPPLY_STATUS_DISCHARGING) {
+		is_charging = false;
 		if (chip->vbat_low_irq_enabled) {
 			disable_irq_wake(chip->batt_irq[VBATT_LOW].irq);
 			disable_irq_nosync(chip->batt_irq[VBATT_LOW].irq);
